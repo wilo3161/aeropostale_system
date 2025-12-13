98bd27f509a8d64a9b51c4d98060263df34321c3
wilo3161 <119455449+wilo3161@users.noreply.github.com>
2025-12-13 18:45:13 -0500

    Add module docstring and update __all__ exports
    Added module docstring and updated exports.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,19 @@
+# modules/__init__.py
+"""
+Módulos del sistema Aeropostale
+"""
 
+from .config_manager import get_config, ConfigManager
+from .error_handler import get_error_handler, ErrorHandler, error_handler_decorator
+from .health_monitor import get_health_monitor, HealthMonitor, init_health_monitoring
+
+__all__ = [
+    'get_config',
+    'ConfigManager',
+    'get_error_handler', 
+    'ErrorHandler',
+    'error_handler_decorator',
+    'get_health_monitor',
+    'HealthMonitor',
+    'init_health_monitoring'
+]
